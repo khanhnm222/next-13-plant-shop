ae03bcb144c9b8ed7d0fe828ef839642a9c0315b
Nguyen Minh Khanh <khanhnguyenm@nashtechglobal.com>
2023-08-20 22:28:15 +0700

    Created Navigation Bar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,91 @@
+'use client';
+
+// react
+import { useState } from 'react';
+// next
+import Link from 'next/link';
+// components
+import Logo from './Logo';
+import Button from './button';
+// icons
+import IconAccount from './icons/IconAccount';
+import IconCart from './icons/IconCart';
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuOpen = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenuOpen = () => {
+    setIsMenuOpen(false);
+  };
+
+  return (
+    <nav className='sticky top-0 w-full bg-white border-b border-gray-100 z-20 px-20'>
+      <div className='wrapper mx-auto h-16 flex items-center justify-between'>
+        <Link href='/' title='Planty shop'>
+          <Logo white={false} />
+        </Link>
+        <div className='flex items-center gap-6'>
+          <ul
+            className={`absolute left-0 p-5 w-full bg-white border-b border-gray-100 z-10 duration-300 ${
+              isMenuOpen
+                ? 'opacity-100 top-16'
+                : 'opacity-0 top-10 pointer-events-none'
+            } lg:static lg:flex lg:gap-7 lg:bg-inherit lg:border-0 lg:p-0 lg:w-fit lg:opacity-100 lg:pointer-events-auto`}
+          >
+            <li className='p-2.5 font-medium hover:text-green-500 duration-150 text-2xl lg:text-base border-b border-gray-100 lg:border-0'>
+              <Link onClick={closeMenuOpen} href='/products' title='Products'>
+                Products
+              </Link>
+            </li>
+            <li className='p-2.5 font-medium hover:text-green-500 duration-150 text-2xl lg:text-base border-b border-gray-100 lg:border-0'>
+              <Link onClick={closeMenuOpen} href='/blogs' title='Blogs'>
+                Blogs
+              </Link>
+            </li>
+            <li className='p-2.5 font-medium hover:text-green-500 duration-150 text-2xl lg:text-base border-b border-gray-100 lg:hidden lg:border-0'>
+              <Link
+                onClick={closeMenuOpen}
+                href='/contact'
+                title='Contact'
+              >
+                Contact
+              </Link>
+            </li>
+            <li className='p-2.5 font-medium hover:text-green-500 duration-150 text-2xl lg:text-base'>
+              <Link onClick={closeMenuOpen} href='/contact' title='Contact'>
+                Contact
+              </Link>
+            </li>
+          </ul>
+
+          <ul className='flex items-center gap-5'>
+            <li className='hover:text-green-500 duration-150'>
+              <Link href='/panel-klienta/zaloguj-sie' title='Moje konto'>
+                <IconAccount />
+              </Link>
+            </li>
+            <li className='hover:text-green-500 duration-150'>
+              <Link href='/koszyk' title='Koszyk'>
+                <IconCart />
+              </Link>
+            </li>
+            <li className='lg:hidden'>
+              <button
+                className={`mobile-menu-btn ${isMenuOpen ? 'active' : ''}`}
+                onClick={handleMenuOpen}
+              >
+                <div className='hamburger-icon'></div>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
