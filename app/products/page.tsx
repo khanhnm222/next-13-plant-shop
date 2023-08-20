b17149468bbe4b8471ac8f6bace75e733c0cb57c
Nguyen Minh Khanh <khanhnguyenm@nashtechglobal.com>
2023-08-20 22:26:54 +0700

    Added page routers

---
+++ app/products/page.tsx
@@ -0,0 +1,14 @@
+// import Header from '@/components/Header'
+import Navbar from '@/components/Navbar'
+import Image from 'next/image'
+
+export default function Products() {
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        Products
+      </main>
+    </>
+  )
+}
+
