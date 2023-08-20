6c25c54961aa8ef49ee3888fe7b76b7ea09c76c2
Nguyen Minh Khanh <khanhnguyenm@nashtechglobal.com>
2023-08-20 22:28:42 +0700

    Updated common components

---
+++ components/button.tsx
@@ -0,0 +1,77 @@
+interface ButtonProps {
+  size?: 'small' | 'large';
+  variant: 'primary' | 'secondary' | 'delete';
+  label?: string;
+  additionalClass?: string;
+  children?: React.ReactNode;
+  icon?: 'left' | 'right' | 'only';
+}
+
+const Button: React.FC<ButtonProps> = ({
+  size,
+  variant,
+  label,
+  additionalClass,
+  children,
+  icon,
+}) => {
+  const getClassName = () => {
+    let baseClass =
+      'rounded-2xl flex items-center justify-center gap-2 font-medium px-4 shadow-sm hover:shadow-md hover:scale-105 active:shadow-sm duration-200';
+
+    let sizeClass = '';
+    switch (size) {
+      case 'small':
+        sizeClass = 'h-8 text-sm';
+        break;
+      case 'large':
+        sizeClass = 'h-12';
+        break;
+      default:
+        sizeClass = 'h-10 text-sm';
+        break;
+    }
+
+    let variantClass = '';
+    switch (variant) {
+      case 'primary':
+        variantClass = 'bg-green-500 active:bg-green-600 text-white';
+        break;
+      case 'secondary':
+        variantClass = 'bg-white active:bg-gray-50';
+        break;
+      case 'delete':
+        variantClass = 'bg-red-600 active:bg-red-700 text-white';
+        break;
+    }
+
+    return `${baseClass} ${sizeClass} ${variantClass} ${additionalClass}`;
+  };
+
+  const buttonContent = (): React.ReactNode => {
+    switch (icon) {
+      case 'left':
+        return (
+          <>
+            {children}
+            {label}
+          </>
+        );
+      case 'right':
+        return (
+          <>
+            {label}
+            {children}
+          </>
+        );
+      case 'only':
+        return children as React.ReactNode;
+      default:
+        return label;
+    }
+  };
+
+  return <button className={getClassName()}>{buttonContent()}</button>;
+};
+
+export default Button;
