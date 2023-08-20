0a593e6d1412852df9305ae6a0bf945cea3a304f
Nguyen Minh Khanh <khanhnguyenm@nashtechglobal.com>
2023-08-20 22:26:25 +0700

    Updated Icon components

---
+++ components/icons/IconTrash.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function IconTrash() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='17'
+      height='16'
+      viewBox='0 0 17 16'
+      fill='none'
+    >
+      <path
+        opacity='0.9'
+        d='M4.19987 12.6667C4.19987 13.4 4.79987 14 5.5332 14H10.8665C11.5999 14 12.1999 13.4 12.1999 12.6667V4.66667H4.19987V12.6667ZM12.8665 2.66667H10.5332L9.86654 2H6.5332L5.86654 2.66667H3.5332V4H12.8665V2.66667Z'
+        fill='currentColor'
+      />
+    </svg>
+  );
+}
