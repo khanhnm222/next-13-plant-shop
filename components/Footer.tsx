1cbcb48052602c5320167db53331f59a65286694
Nguyen Minh Khanh <khanhnguyenm@nashtechglobal.com>
2023-08-20 23:27:20 +0700

    Updated Footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,31 +27,31 @@ export default function Footer() {
 
   // footer
   return (
-    <footer className='footer mt-20 pt-14 pb-10 bg-black text-white'>
+    <footer className='footer mt-20 pt-14 pb-10 bg-primary text-white'>
       <div className='wrapper flex flex-col gap-10 md:items-center'>
         <Logo white={true} />
         <div className='social-media flex flex-col gap-5 md:items-center'>
           <div className='flex gap-7'>
             <Link
-              href='/sklep'
-              title='Sklep'
+              href='/products'
+              title='Products'
               className='hover:text-green-500 duration-200'
             >
-              Soki
+              Products
             </Link>
             <Link
-              href='/o-nas'
-              title='O nas'
+              href='/blogs'
+              title='Blogs'
               className='hover:text-green-500 duration-200'
             >
-              O nas
+              Blogs
             </Link>
             <Link
-              href='/kontakt'
-              title='Kontakt'
+              href='/contact'
+              title='Contact'
               className='hover:text-green-500 duration-200'
             >
-              Kontakt
+              Contact
             </Link>
           </div>
           <div className='flex gap-5'>
@@ -83,16 +83,16 @@ export default function Footer() {
             {footerData.description}
           </p>
           <div className='credits flex flex-col md:flex-row gap-2.5 md:items-center'>
-            <p className='text-gray-200'>©2023 Healthy Juice</p>
+            <p className='text-gray-200'>©2023 Plant shop</p>
             <Link
-              href='/polityka-prywatnosci'
-              title='Polityka prywatności'
+              href='/'
+              title='Primary Policy'
               className='text-gray-200'
             >
-              Polityka prywatności
+              Primary Policy
             </Link>
-            <Link href='/regulamin' title='Regulamin' className='text-gray-200'>
-              Regulamin
+            <Link href='/' title='Terms and Conditions' className='text-gray-200'>
+              Terms and Conditions
             </Link>
           </div>
         </div>
