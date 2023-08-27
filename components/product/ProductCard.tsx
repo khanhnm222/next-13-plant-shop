8fff96d8d2f0b20538b2de6330f90dd6a4badf05
Nguyen Minh Khanh <khanhnguyenm@nashtechglobal.com>
2023-08-27 21:00:17 +0700

    Updated Product card

--- components/product/ProductCard.tsx
+++ components/product/ProductCard.tsx
@@ -2,34 +2,34 @@ import { getDiscountedPricePercentage } from "@/utils/helpers";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-const ProductCard = ({ data: { attributes: p } }: any) => {
+const ProductCard = ({ data: { attributes: product } }: any) => {
   return (
     <Link
-      href={`/product/${p.slug}`}
+      href={`/products/${product.id}`}
       className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer"
     >
       <Image
-        width={500}
-        height={500}
-        src={p.thumbnail.data.attributes.url}
-        alt={p.name}
+        width={400}
+        height={550}
+        src={product.thumbnail.data.attributes.url}
+        alt={product.name}
       />
       <div className="p-4 text-black/[0.9]">
-        <h2 className="text-lg font-medium">{p.name}</h2>
+        <h2 className="text-lg font-medium">{product.name}</h2>
         <div className="flex items-center text-black/[0.5]">
           <p className="mr-2 text-lg font-semibold">
-            &#8377;{p.price}
+            &#8377;{product.price}
           </p>
 
-          {p.original_price && (
+          {product.original_price && (
             <>
               <p className="text-base  font-medium line-through">
-                &#8377;{p.original_price}
+                &#8377;{product.original_price}
               </p>
               <p className="ml-auto text-base font-medium text-primary">
                 {getDiscountedPricePercentage(
-                  p.original_price,
-                  p.price
+                  product.original_price,
+                  product.price
                 )}
                 % off
               </p>
