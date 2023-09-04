b1aba19410f3ef6301002f919cc8b1149f8f494d
Nguyen Minh Khanh <khanhnguyenm@nashtechglobal.com>
2023-09-05 02:55:22 +0700

    Implement Blog page UI

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,12 +1,31 @@
-// import Header from '@/components/Header'
-import Navbar from '@/components/navigation/Navbar'
-import Image from 'next/image'
+import BlogCard from "@/components/blog/BlogCard";
+import Breadcrumb from "@/components/common/Breadcrumb";
+import Pagination from "@/components/common/Pagination";
 
 export default function Blogs() {
+  const blogItems = [1,2,3,4,5,6]
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        Blogs
+      <main className="flex min-h-[53vh] flex-col items-center justify-between p-24">
+        <div className="pt-2 col-span-9 text-brand-green">
+          <div className="pb-2 text-center">
+            <h3 className="text-xl font-extralight leading-none">The Shop</h3>
+            <h2 className="pd-2 text-4xl font-bold tracking-tighter font-heading leading-none mb-1">Explore the blogs</h2>
+          </div>
+        </div>
+        <div className="px-6 self-start font-semibold">
+          <Breadcrumb />
+        </div>
+        <section className=" dark:bg-gray-900">
+          <div className="container px-6 pb-10 mx-auto">
+            <div className="grid grid-cols-1 gap-10 mt-5 mb-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              {blogItems && blogItems.map(blog => (
+                <BlogCard key={blog}/>
+              ))}
+            </div>
+          </div>
+        </section>
+        <Pagination className="flex justify-center mt-16" />
       </main>
     </>
   )
