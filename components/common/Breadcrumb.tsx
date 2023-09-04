29330c171417bda5e8bea8c1a9a029a57654912c
Nguyen Minh Khanh <khanhnguyenm@nashtechglobal.com>
2023-09-05 02:56:13 +0700

    Update Breadcrumb component

--- components/common/Breadcrumb.tsx
+++ components/common/Breadcrumb.tsx
@@ -3,8 +3,8 @@ const Breadcrumb = () => {
    <>
     <nav aria-label="breadcrumb"> 
       <ol className="flex space-x-2">
-        <li><a href="/" className="after:content-['/'] after:ml-2 text-gray-600 hover:text-primary">Home</a></li>
-        <li className="text-primary" aria-current="page">Pruducts</li> 
+        <li><a href="/" className="after:content-['/'] after:ml-2 text-gray-600 hover:text-brand-green">Home</a></li>
+        <li className="text-brand-green" aria-current="page">Products</li> 
       </ol>
     </nav>
    </>
