273dfd2ee3641511f643f3c4c565997be11749f7
Nguyen Minh Khanh <khanhnguyenm@nashtechglobal.com>
2023-09-06 01:03:50 +0700

    Update configs

--- next.config.js
+++ next.config.js
@@ -8,6 +8,18 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'bloomscape.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.freepik.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -48,8 +48,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/line-clamp'),
-  ],
+  plugins: [],
 }
 export default config
