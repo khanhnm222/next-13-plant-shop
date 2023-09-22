ce5f6851c902a5ba6fb69efe16716720d66c15e4
Nguyen Minh Khanh <khanhnguyenm@nashtechglobal.com>
2023-09-23 01:07:38 +0700

    Create Icons component

---
+++ components/icons/index.ts
@@ -0,0 +1,41 @@
+import IconAccount from "./IconAccount";
+import IconArrowRight from "./IconArrowRight";
+import IconCart from "./IconCart";
+import IconChevronDown from "./IconChevronDown";
+import IconClose from "./IconClose";
+import IconEdit from "./IconEdit";
+import IconFacebook from "./IconFacebook";
+import IconFAQ from "./IconFAQ";
+import IconFilter from "./IconFilter";
+import IconGoogle from "./IconGoogle";
+import IconInstagram from "./IconInstagram";
+import IconNext from "./IconNext";
+import IconNextOrange from "./IconNextOrange";
+import IconPrev from "./IconPrev";
+import IconSearch from "./IconSearch";
+import IconSend from "./IconSend";
+import IconSort from "./IconSort";
+import IconTrash from "./IconTrash";
+import IconTwitter from "./IconTwitter";
+
+export {
+  IconAccount,
+  IconArrowRight,
+  IconCart,
+  IconChevronDown,
+  IconClose,
+  IconEdit,
+  IconFacebook,
+  IconFAQ,
+  IconFilter,
+  IconGoogle,
+  IconInstagram,
+  IconNext,
+  IconNextOrange,
+  IconPrev,
+  IconSearch,
+  IconSend,
+  IconSort,
+  IconTrash,
+  IconTwitter,
+}
