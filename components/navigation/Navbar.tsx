5c4175fb324b7a860be2980959b677a53f914efd
Nguyen Minh Khanh <khanhnguyenm@nashtechglobal.com>
2023-09-23 21:51:21 +0700

    Update Navigation bar component

--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -29,7 +29,7 @@ const Navbar = () => {
           <Logo white={false} />
         </Link>
         <div className='flex items-center gap-6'>
-          <div className="w-full bg-white hidden md:flex items-center gap-x-1 border-[1px] border-lightText/50 rounded-full px-4 py-1.5 focus-within:border-white group h-[35px]">
+          <div className="w-full bg-white hidden md:flex items-center gap-x-1 border-[1px] border-lightText/50 rounded-full px-4 py-1.5 focus-within:border-brand-beige group h-[35px]">
             <IconSearch className="text-gray-500 group-focus-within:text-darkText duration-200 w-[25px] h-[auto] opacity-40" />
             <input
               type="text"
@@ -44,17 +44,17 @@ const Navbar = () => {
                 : 'opacity-0 top-10 pointer-events-none'
             } lg:static lg:flex lg:gap-7 lg:bg-inherit lg:border-0 lg:p-0 lg:w-fit lg:opacity-100 lg:pointer-events-auto`}
           >
-            <li className='nav-underline p-2.5 font-medium tracking-tighter hover:text-orange-200 duration-150 text-2xl lg:text-base border-b border-gray-100 lg:border-0'>
+            <li className='nav-underline p-2.5 font-medium tracking-tighter hover:text-brand-beige duration-150 text-2xl lg:text-base border-b border-gray-100 lg:border-0'>
               <Link onClick={closeMenuOpen} href='/products' title='Products'>
                 Products
               </Link>
             </li>
-            <li className='nav-underline p-2.5 font-medium hover:text-orange-200 duration-150 text-2xl lg:text-base border-b border-gray-100 lg:border-0'>
+            <li className='nav-underline p-2.5 font-medium hover:text-brand-beige duration-150 text-2xl lg:text-base border-b border-gray-100 lg:border-0'>
               <Link onClick={closeMenuOpen} href='/blogs' title='Blogs'>
                 Blogs
               </Link>
             </li>
-            <li className='nav-underline p-2.5 font-medium hover:text-orange-200 duration-150 text-2xl lg:text-base'>
+            <li className='nav-underline p-2.5 font-medium hover:text-brand-beige duration-150 text-2xl lg:text-base'>
               <Link onClick={closeMenuOpen} href='/contact' title='Contact'>
                 Contact
               </Link>
