fe6f8c7373c1cf42a14f729e53d5329c2838f9ab
Nguyen Minh Khanh <khanhnguyenm@nashtechglobal.com>
2023-09-23 21:49:30 +0700

    Update Product Recommadation section

--- components/product/ProductTeaser.tsx
+++ components/product/ProductTeaser.tsx
@@ -1,37 +1,15 @@
-import Image from "next/image"
-import Link from "next/link"
-import Button from "../common/Button"
-import RankingStar from "./RankingStar"
+import ProductCard from "./ProductCard"
 
 const ProductTeaser = () => {
+  const data = [1, 2, 3, 4]
   return (
-    <div>
-      <article>
-        <Link href={"/products/1"} className='group relative'>
-        <div className="flex justify-center aspect-w-1 aspect-h-1 group overflow-hidden relative">
-          <Image
-            src='/monstera.webp'
-            alt="plant"
-            loading="lazy"
-            width={250}
-            height={350}
-            className='h-auto group-hover:scale-110 duration-200'
-          />
-        </div>
-        </Link>
-      </article>
-      <div className="grid text-center">
-        <h3 className="text-brand-green text-xl font-medium truncate block p-5">Montera Diamon</h3>
-        <div className="text-lg text-brand-green flex justify-center mb-5 flex-wrap gap-[5px]">
-          <RankingStar />
-          <span>125.000 VND</span>
-        </div>
-        <Button variant={"primary"} icon='only'>
-          <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 mx-1" viewBox="0 0 20 20" fill="currentColor">
-            <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
-          </svg>
-          Add to cart
-        </Button>
+    <div className="mt-10">
+      <hr className="h-px my-8 bg-primary border-0 dark:bg-gray-700 opacity-[0.5]"/>
+      <p className="text-xl py-1 font-semibold text-brand-green">YOU MAY ALSO LIKE</p>
+      <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-4 xl:grid-cols-4 gap-4 mt-5 justify-items-center">
+        {data?.map((item: any) => (
+          <ProductCard key={item}/>
+        ))}
       </div>
     </div>
   )
