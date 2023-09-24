815d74064b13c9742161a0c4147f7d0b735f2a95
Nguyen Minh Khanh <khanhnguyenm@nashtechglobal.com>
2023-09-25 01:16:50 +0700

    Create Advertising section

---
+++ components/home/Advertising/index.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+const Advertising = () => {
+  return (
+    <div className="container pb-16 pt-24">
+      <a href="#">
+        <Image src="/natural-plants-banner-buying-guide.avif" alt="ads" className="w-full" width={1024} height={400} />
+      </a>
+    </div>
+  )
+}
+
+export default Advertising
