32e2696949aea5475627eac7b26eaf3c5595c1fa
Nguyen Minh Khanh <khanhnguyenm@nashtechglobal.com>
2023-09-25 01:14:27 +0700

    Update new sections on Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,25 @@
-import Subscribe from '@/components/home/Subscribe'
-import TopCover from '@/components/home/TopCover'
+"use client";
+
+import Carousel from "@/components/home/Carousel";
+import Subscribe from "@/components/home/Subscribe";
+import TopCover from "@/components/home/TopCover";
+import Features from "@/components/home/Features";
+import Advertising from "@/components/home/Advertising";
+import ProductTeaser from "@/components/product/ProductTeaser";
 
 export default function Home() {
   return (
     <>
-      <header className='home-header w-full'>
+      <header className="home-header w-full">
         <TopCover />
       </header>
+      <Carousel />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <Features />
+        <ProductTeaser disableHeader className="container"/>
+        <Advertising />
         <Subscribe />
       </main>
     </>
-  )
+  );
 }
