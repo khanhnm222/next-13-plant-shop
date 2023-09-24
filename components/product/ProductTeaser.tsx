d306531807e0a1efbf68d097d71531c8a89347ca
Nguyen Minh Khanh <khanhnguyenm@nashtechglobal.com>
2023-09-25 01:17:52 +0700

    Update props of Product teaser component

--- components/product/ProductTeaser.tsx
+++ components/product/ProductTeaser.tsx
@@ -1,14 +1,24 @@
 import ProductCard from "./ProductCard"
 
-const ProductTeaser = () => {
+interface ProductTeaserProps {
+  className?: string;
+  disableHeader?: boolean;
+}
+
+const ProductTeaser = ({ className, disableHeader = false }: ProductTeaserProps) => {
   const data = [1, 2, 3, 4]
   return (
-    <div className="mt-10">
-      <hr className="h-px my-8 bg-primary border-0 dark:bg-gray-700 opacity-[0.5]"/>
-      <p className="text-xl py-1 font-semibold text-brand-green">YOU MAY ALSO LIKE</p>
+    <div className={className ? `${className} mt-10` : 'mt-10'}>
+      { !disableHeader && (
+          <>
+            <hr className="h-px my-8 bg-primary border-0 dark:bg-gray-700 opacity-[0.5]"/>
+            <p className="text-xl py-1 font-semibold text-brand-green">YOU MAY ALSO LIKE</p>
+          </>
+        )
+      }
       <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-4 xl:grid-cols-4 gap-4 mt-5 justify-items-center">
         {data?.map((item: any) => (
-          <ProductCard key={item}/>
+          <ProductCard key={item} isTeaser/>
         ))}
       </div>
     </div>
