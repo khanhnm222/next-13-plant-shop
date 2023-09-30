c021f735ceb22a24305d1fc845f790326ad5f04e
Nguyen Minh Khanh <khanhnguyenm@nashtechglobal.com>
2023-09-30 22:48:42 +0700

    Add new icon components

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -1,25 +1,28 @@
-import IconAccount from "./IconAccount";
-import IconArrowRight from "./IconArrowRight";
-import IconCart from "./IconCart";
-import IconCashback from "./IconCashback";
-import IconChevronDown from "./IconChevronDown";
-import IconClose from "./IconClose";
-import IconDelivery from "./IconDelivery";
-import IconEdit from "./IconEdit";
-import IconFacebook from "./IconFacebook";
-import IconFAQ from "./IconFAQ";
-import IconFilter from "./IconFilter";
-import IconGoogle from "./IconGoogle";
-import IconInstagram from "./IconInstagram";
-import IconNext from "./IconNext";
-import IconNextOrange from "./IconNextOrange";
-import IconPrev from "./IconPrev";
-import IconSearch from "./IconSearch";
-import IconSend from "./IconSend";
-import IconServiceHour from "./IconServiceHour";
-import IconSort from "./IconSort";
-import IconTrash from "./IconTrash";
-import IconTwitter from "./IconTwitter";
+import IconAccount from "./IconAccount"
+import IconArrowRight from "./IconArrowRight"
+import IconCart from "./IconCart"
+import IconCashback from "./IconCashback"
+import IconChevronDown from "./IconChevronDown"
+import IconClose from "./IconClose"
+import IconComment from "./IconComment"
+import IconDelivery from "./IconDelivery"
+import IconEdit from "./IconEdit"
+import IconFacebook from "./IconFacebook"
+import IconFAQ from "./IconFAQ"
+import IconFilter from "./IconFilter"
+import IconGoogle from "./IconGoogle"
+import IconInstagram from "./IconInstagram"
+import IconLike from "./IconLike"
+import IconNext from "./IconNext"
+import IconNextOrange from "./IconNextOrange"
+import IconPrev from "./IconPrev"
+import IconSearch from "./IconSearch"
+import IconSend from "./IconSend"
+import IconServiceHour from "./IconServiceHour"
+import IconSort from "./IconSort"
+import IconTrash from "./IconTrash"
+import IconTwitter from "./IconTwitter"
+import IconUpArrow from "./IconUpArrow"
 
 export {
   IconAccount,
@@ -28,6 +31,7 @@ export {
   IconCashback,
   IconChevronDown,
   IconClose,
+  IconComment,
   IconDelivery,
   IconEdit,
   IconFacebook,
@@ -35,6 +39,7 @@ export {
   IconFilter,
   IconGoogle,
   IconInstagram,
+  IconLike,
   IconNext,
   IconNextOrange,
   IconPrev,
@@ -44,4 +49,5 @@ export {
   IconSort,
   IconTrash,
   IconTwitter,
+  IconUpArrow,
 }
